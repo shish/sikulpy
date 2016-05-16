f1c12dfe921d64f2581745fa7b0ca68360c8f4d7
Shish <shish@shishnet.org>
2016-05-16 20:12:44 +0100

    set custom similarity

--- sikuli/find.py
+++ sikuli/find.py
@@ -6,8 +6,8 @@ import logging
 Settings.ImagePaths = ["."]
 
 
-def run(img):
-    p = Pattern(img).similar(0.7)
+def run(img, similarity):
+    p = Pattern(img).similar(similarity)
     s = Screen(0)
     s._debug = True
     s.exists(p)
@@ -17,12 +17,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', default=False, action='store_true')
     parser.add_argument('-s', '--scale', type=float, default=1.0)
+    parser.add_argument('-S', '--similarity', type=float, default=0.7)
     parser.add_argument('img')
     args = parser.parse_args()
     if args.debug:
         logging.getLogger("sikuli").setLevel(logging.DEBUG)
     Settings.Scale = args.scale
-    run(args.img)
+    run(args.img, args.similarity)
     return 0
 
 
